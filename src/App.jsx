b82954a4bb2de2c19d8770fdcd5e1b76b7dfca60
syed qasim <qasimsyed563@gmail.com>
2025-03-05 22:43:00 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Section/Header.jsx'
+import Hero from './Section/Hero.jsx'
+import Features from './Section/Features.jsx'
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header/>
+      <Hero/>
+      <Features/>
+    </main>
+  )
+}
+
+export default App
