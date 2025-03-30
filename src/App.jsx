8511c9adb580e69426331f366fd2bacfc849ebf9
syed qasim <qasimsyed563@gmail.com>
2025-03-30 12:39:51 +0500

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import Header from './Section/Header.jsx'
 import Hero from './Section/Hero.jsx'
 import Features from './Section/Features.jsx'
+import Price from './Section/Price.jsx'
+import Faq from './Section/Faq.jsx'
+import Testimonilas from './components/Testimonilas.jsx'
+import Download from './Section/Download.jsx'
+import Footer from './Section/Footer.jsx'
 const App = () => {
   return (
     <main className='overflow-hidden'>
       <Header/>
       <Hero/>
       <Features/>
+      <Price/>
+      <Faq/>
+      <Testimonilas />
+      <Download />
+      <Footer/>
     </main>
   )
 }
---
+++ src/components/images/index.js
@@ -0,0 +1,10 @@
+// import Android from "./Android.svg";
+// import Ios from "./Ios.svg";
+// import Windows from "./Windows.svg";
+// import Web from "./Web.svg";
+
+
+// export const playstore = Android;
+// export const apple = Ios;
+// export const microsoft = Windows;
+// export const website = Web;
